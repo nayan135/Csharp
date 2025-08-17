f5ac8a1508c4f239feef0ce0ecab35f8bd7e6cbc
NAYAN ACHARYA <nayan374@proton.me>
2025-08-17 08:09:12 +0545

    Add project files.

---
+++ odd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Odd
+{
+    private void OddFun()
+    {
+        Console.WriteLine("Enter a number");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public static void main(string[] args)
+    {
+        Odd o = new Odd();
+        o.OddFun();
+    }
+}   
+
