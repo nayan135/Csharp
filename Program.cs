ecaed345c6ff9f9d3b0b4bb4e8fcf2a32b23e3ac
NAYAN ACHARYA <nayan374@proton.me>
2025-09-15 22:47:43 +0545

    updatee

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row, col;
+        Console.WriteLine("Enter the size of matrix(row)");
+        row = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the size of matrix(column)");
+        col = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[row, col];
+        Console.WriteLine("Enter the elements of matrix");
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        //greatest value in the matrix
+        int greatest = matrix[0, 0];
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (matrix[i, j] > greatest)
+                {
+                    greatest = matrix[i, j];
+                }
+            }
+        }
+        Console.WriteLine("The greatest value in the matrix is " + greatest);
+    }
+}
