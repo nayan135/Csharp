571070868833665e4315c32bf7fa21e381fc6b1c
NAYAN ACHARYA <100992818+nayan135@users.noreply.github.com>
2026-01-07 20:56:37 +0545

    ntg ig

---
+++ jaggedarray.cs
@@ -0,0 +1,15 @@
+//paxi garxu yoooo 
+using System;
+class Jagged
+{
+    public static void Main()
+    {
+        int[][] marks= new int[3][];
+       
+
+       marks[0]= new int[2];
+       marks[1]= new int[3];
+       marks[2]= new int[4];
+
+    }
+    }
