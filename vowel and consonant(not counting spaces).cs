f83960b0bf14137a3946376ce670322fe98c6824
NAYAN ACHARYA <100992818+nayan135@users.noreply.github.com>
2026-01-07 21:51:00 +0545

    vowel and consonant (excluding space)

---
+++ vowel and consonant(not counting spaces).cs
@@ -0,0 +1,39 @@
+using System;
+class vowel
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string n= Console.ReadLine().ToLower();
+        char[] vo={'a','e','i','o','u'};
+        int vcount=0;
+        int ccount=0;
+       
+        for(int i=0; i<n.Length; i++)
+        {
+             bool isvowel= false;
+            for (int j=0; j<vo.Length; j++)
+            {
+                
+            
+            if (n[i] == vo[j])
+            {
+                isvowel=true;
+                
+            }
+             
+            }
+                if (isvowel){
+                vcount++;
+                
+            }
+                else if (Char.IsLetter(n[i])){
+                    ccount++;
+                }
+            }
+        
+
+    Console.WriteLine($"vowels: {vcount} && consonant: {ccount}");
+    }
+
+}
