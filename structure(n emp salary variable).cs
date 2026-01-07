d7ba49469ae093df20e805cfc2ccf9d5b5b1c042
NAYAN ACHARYA <100992818+nayan135@users.noreply.github.com>
2026-01-07 21:26:25 +0545

    changess

---
+++ structure(n emp salary variable).cs
@@ -0,0 +1,47 @@
+using System;
+struct Employe
+{
+    public int id;
+    public string name;
+    public double salary;
+
+
+
+    static void Main(){
+     Console.WriteLine("Enter the no. of Employee");
+    int n= Convert.ToInt32(Console.ReadLine());
+    Employe[] employee = new Employe[n];
+
+    for (int i =0; i<n; i++)
+        {
+            Console.WriteLine($"Enter the id of {i+1} employee");
+            employee[i].id= Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine($"Enter the name of {i+1} employee");
+             employee[i].name= Console.ReadLine();
+         Console.WriteLine($"Enter the salary of {i+1} employee");
+            employee[i].salary= Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.WriteLine("====== Details of the Employee ======");
+        for (int i=0; i<n; i++)
+        {
+            Console.WriteLine($"ID:{employee[i].id}");
+            Console.WriteLine($"Name:{employee[i].name}");
+            Console.WriteLine($"Salary:{employee[i].salary}");
+            
+            
+        }
+
+        Console.WriteLine("=== Salary Limit print=====");
+    double salary =21000.00;
+        for(int i=0; i < n; i++)
+        {
+            if (employee[i].salary <= salary)
+            {
+                 Console.WriteLine($"ID:{employee[i].id}");
+            Console.WriteLine($"Name:{employee[i].name}");
+            Console.WriteLine($"Salary:{employee[i].salary}");
+            }
+        }
+    }
+}
